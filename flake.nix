c0687dce918a515e0442114b1fc534434b8791b3
Miouzora <alexandre.franquet@epitech.eu>
2024-04-13 18:52:03 +0200

    Add buildPhase and installPhase in flake

--- flake.nix
+++ flake.nix
@@ -7,13 +7,14 @@
   outputs = { self, nixpkgs, flake-utils, zig-overlay }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
+        # Refer to https://github.com/mitchellh/zig-overlay if you want to use a specific version of Zig
         zigPackage = zig-overlay.packages.${system}.master;
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
         formatter = pkgs.nixpkgs-fmt;
         devShells.default = pkgs.mkShell {
-          name = "zix";
+          name = "name_of_your_project";
           packages = with pkgs; [
             raylib
           ];
@@ -22,17 +23,19 @@
           ];
         };
         packages.default = pkgs.stdenv.mkDerivation {
-          name = "Zix";
+          name = "name_of_your_project";
           src = ./.;
 
           XDG_CACHE_HOME = "${placeholder "out"}";
+
+          buildInputs = [ pkgs.raylib ];
           buildPhase = ''
             ${zigPackage}/bin/zig build
           '';
 
           installPhase = ''
             ${zigPackage}/bin/zig build install --prefix $out
-            rm -rf $out/zig
+            rm -rf $out/zig # remove cache
           '';
         };
       });
